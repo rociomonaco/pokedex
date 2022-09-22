c768e09d183818ae7b3d470eb130aa1bcb6908e5
nicolasarielmaldonado <nicolas.ariel.maldonado@gmail.com>
2022-09-22 16:05:10 -0300

    crear pokemon

--- createPokemon.php
+++ createPokemon.php
@@ -6,28 +6,25 @@
     $tipo1 = $_POST["tipo1"];
     $tipo2 = empty($_POST["tipo2"]) ? null : $_POST["tipo2"];
     $numero = $_POST["numero"];
-    $img = $_FILES["img"];
+    $img = subir_img($_FILES["img"]);
     $descripcion = $_POST["descripcion"];
     $peso = $_POST["peso"];
     $altura = $_POST["altura"];
 
-
-    var_dump($_POST);
-    // validamos de que los campos no esten vacios para la insercion a la bd
     $error = false; 
     $campos = array($nombre,$tipo1,$numero,$descripcion,$peso,$altura);
 
-     foreach ($campos as $clave => $campo) {
+     foreach ($campos as $campo) {
         if ($error){
             die('ERROR VALIDACION!');
         } else {
-            $error = validacion($campo, $tipos[$clave]);
+            $error = validacion($campo);
         }
     };
 
-    echo "TODO BIEN";
+    insertar_pokemon($nombre,$tipo1,$tipo2,$numero,$img,$descripcion,$peso,$altura);
 
-    function validacion($variable, $tipo){
+    function validacion($variable){
         $error = false;
         if (empty($variable)){
             $error = true;
@@ -35,27 +32,20 @@
         } 
     }
 
- /*    /if(!empty($nombre) && !empty($tipo1) && !empty($numero) && !empty($img) && !empty($descripcion) && !empty($peso) && !empty($altura)){
-
-        subir_img($img);
-
-     }
-     */
-
     function subir_img($img){
         $nombre = $img["name"];
         $size = $img["size"];
         $type = $img["type"];
         
-      /*   $carpeta_destino = $_SERVER["DOCUMENT_ROOT"] . "/pokedex/img/";
-        move_uploaded_file($img["tmp_name"],$carpeta_destino . $nombre); */
+        $carpeta_destino = $_SERVER["DOCUMENT_ROOT"] . "/pokedex/img/";
+        move_uploaded_file($img["tmp_name"],$carpeta_destino . $nombre);
+        return $nombre;
     }
 
     function insertar_pokemon($nombre,$tipo1,$tipo2,$numero,$img,$descripcion,$peso,$altura){
         $db = new Database();
         $sql = "INSERT INTO `pokemones`(`nombre`, `tipo1`, `tipo2`, `numero`, `img`, `descripcion`, `peso`, `altura`) 
-        VALUES ('$nombre','$tipo1','$tipo2','$numero','$img','$descripcion',',$peso','$altura')";
-        $db->query($sql);
-        header("location: index.php");
+        VALUES ('$nombre','$tipo1','$tipo2','$numero','$img','$descripcion','$peso','$altura')";
+        $db->execute($sql);
         
     }
